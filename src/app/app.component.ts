c4949a6898453226e8c089fafd50bb963f61c01c
Steven Rivera <ghostszmusic@gmail.com>
2021-07-03 01:54:30 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Observable } from 'rxjs';
+import {User, UserData} from "./user-data.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  get currentUser(): UserData {
+    return this._currentUser;
+  }
+
+  set currentUser(value: UserData) {
+    this._currentUser = value;
+  }
   title = 'ghs-restapi-frontend';
+  users: any;
+  tempUsers: any;
+  tempUser: any;
+  // baseUrl = "http://localhost:3000/";
+  baseUrl = "https://ghs-rest-api-test.herokuapp.com/";
+  selectedID: any;
+  private _currentUser!: UserData;
+
+  headers= new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*');
+
+  constructor(private http: HttpClient) {
+
+    this.http.get(this.baseUrl+ "api/getUsers", { 'headers': this.headers }).subscribe(data=> {
+      this.tempUsers = data;
+      this.users = this.tempUsers.users;
+    });
+  }
+
+  getUserById(id:any) {
+    const body = {
+      id: id
+    };
+    this.http.post<UserData>(this.baseUrl +"api/sendID", body,{'headers':this.headers , observe: 'response'}).subscribe(($:any)=>{
+      this.currentUser = $.body;
+      console.log(this.currentUser.firstName);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
